8d5f0ed53d208899db0919abeae106ee28f7916d
ShaunAndersonAZ <shaunandersonaz@gmail.com>
2020-04-01 08:54:51 -0700

    Adding compute instance

--- main.tf
+++ main.tf
@@ -11,3 +11,14 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_compute_isntance" "vm_instance" {
+  name          = "terraform-instance"
+  machine_type  = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+}
