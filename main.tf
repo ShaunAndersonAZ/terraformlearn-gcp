846e552109cdb5b2f4acdee848aaa582478d2901
ShaunAndersonAZ <shaunandersonaz@gmail.com>
2020-04-01 09:16:51 -0700

    add tags

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_instance" "vm_instance" {
   name          = "terraform-instance"
   machine_type  = "f1-micro"
+  tags = [web, dev]
 
   boot_disk {
     initialize_params {
@@ -25,7 +26,7 @@ resource "google_compute_instance" "vm_instance" {
   network_interface {
     network = google_compute_network.vpc_network.name
     access_config {
-      
+
     }
   }
 }
